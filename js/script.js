21bbb9ebbf05b107dea70f13682e669b8432b3ba
M4ximka123 <max.show.200122@gmail.com>
2022-10-02 14:45:52 +0300

    add tabs

--- js/script.js
+++ js/script.js
@@ -4,4 +4,16 @@ $(function(){
       prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/icons/arrow-prev.svg" alt="стрелка на право"></button>',
       nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/icons/arrow-next.svg" alt="стрелка на право"></button>'
    });
+
+   $('.search__tabs-item').on('click', function(e){
+      e.preventDefault();
+
+      $('.search__tabs-item').removeClass('search__tabs-item--active');
+      $('.search__content-item').removeClass('search__content-item--active');
+
+      $(this).addClass('search__tabs-item--active');
+      $($(this).attr('href')).addClass('search__content-item--active');
+
+   });
+
 }); 
